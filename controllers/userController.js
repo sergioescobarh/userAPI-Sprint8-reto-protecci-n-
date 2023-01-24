5a5e6c9ccc3a0d36e0f4c652127aa7e6bf1bdcc9
william serna <wserna880@gmail.com>
2023-01-23 20:02:00 -0500

    adding put, delete and post methods

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,10 +4,73 @@ const Op = DB.Sequelize.Op;
 module.exports = {
     list: (req, res) => {
         DB
-        .User
-        .findAll()
-        .then(users => {
-         return res.json(users)
+            .User
+            .findAll()
+            .then(users => {
+                return res.status(200).json({
+                    total: users.length,
+                    data: users,
+                    status: 200
+                })
+            })
+    },
+    idUser: (req, res) => {
+        DB
+            .User
+            .findByPk(req.params.id)
+            .then(user => {
+                return res.status(200).json({
+                    data: user,
+                    status: 200
+                })
+            })
+    },
+    userCreate: (req, res) => {
+        DB
+            .User
+            .create(req.body)
+            .then(user => {
+                return res.status(200).json({
+                    data: user,
+                    status: 200,
+                    created: "ok"
+                })
+            })
+    },
+    userDelete: (req, res) => {
+        DB
+            .User
+            .destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(response => {
+                return res.json(response)
+
+            })
+    },
+    search: (req, res) => {
+        DB
+            .User
+            .findAll({
+                where: {
+                    firtName: { [Op.like]: '%' + req.query.keyword + '%' }
+                }
+            })
+            .then(users => {
+                if (users.length > 0) {
+                    return res.status(200).json(users);
+                }
+                return res.status(200).json('No existe el usuario');
+            })
+    },
+    editUser: (req, res) => {
+        DB.User.update({ ...req.body }, { where: { id: req.params.id } }).then(user => {
+            res.json({
+                data: user,
+                status: 200
+            })
         })
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,12 @@ var router = express.Router();
 const controller = require('../controllers/userController');
 
 router.get('/', controller.list);
+router.get('/search', controller.search);
+router.get('/:id', controller.idUser);
+router.post('/', controller.userCreate);
+router.put('/:id', controller.editUser);
+router.delete('/:id', controller.userDelete);
+
 
 
 module.exports = router;
